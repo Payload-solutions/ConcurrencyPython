2ef70ed99961d02a06080cf7b3865a66ae35c611
Arturo Negreiros <anegreiross@outlook.com>
2021-07-28 10:29:19 -0500

    added new features

--- chapter2/handling_threads.py
+++ chapter2/handling_threads.py
@@ -2,7 +2,7 @@
 
 
 import threading
-import time 
+import time
 import random
 
 
---
+++ index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import threading
+import time
+import random
+from math import *
+
+
+def init_paralllelism():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
