c3a2260e36480ae54a9898826687f1d68839311d
Arturo Negreiros <anegreiross@outlook.com>
2021-07-28 11:46:19 -0500

    added new features

--- index.py
+++ index.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
 
-import threading
-import time
-import random
-from math import *
+from typing import Dict, List, Tuple
 
 
-def init_paralllelism():
-    pass
+def pytuples():
+    numbers: Tuple[int, float, int] = (1, 0.51, 1)
+
+def implement_annotations():
+    positives: List[int]
+
+    users: Dict[str, int] = {
+    "Ecuador": 1,
+    "Mexico": 34,
+    "Colombia": 45,
+    }
+
+    print(type(users))
 
 def main():
-    pass
+    implement_annotations()
 
 if __name__ == "__main__":
     main()
