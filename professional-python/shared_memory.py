4f9d06415481437ffae5c38ec7c33456393563fe
Arturo0911 <anegreiross@outlook.com>
2021-08-09 01:22:15 -0500

    ok

--- professional-python/shared_memory.py
+++ professional-python/shared_memory.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python3
 
-# from multiprocessing import shared_memory
+import multiprocessing
+from random import randint
 
 
 
-# def main():
-#     memory = shared_memory.SharedMemory(create=True, size=10)
-#     print(type(memory.buf))
-#     print(len(memory.buf))
+def squared_list(mylist, result, square_sum):
+    """
+    function to square a given list
+    """
 
-import ray
-import time
-
-ray.init()
-
-
-@ray.remote
-def f(x):
-    time.sleep(1)
-    return x
-
-
-# start 4 tasks in paralell.
-result_ids = []
-for i in range(4):
-    result_ids.append(f.remote(i))
 
 
 
 def main():
     pass
-
 
 
 if __name__ == "__main__":
