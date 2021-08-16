79b57237d98cf815742468ff5c1ef16a026da1a0
Arturo0911 <anegreiross@outlook.com>
2021-08-16 13:09:06 -0500

    linked list implemented

---
+++ double_link_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3.9
+
+class Node:
+    def __init__(self, data, next=None) -> None:
+        self.data = data
+        self.next = next
+
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
--- pynode.py
+++ pynode.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3.9
 
+# from random import randint
+# import time
+# from link_list import SingleLinkList
 
 class Node:
 
@@ -8,9 +11,31 @@ class Node:
         self.next = next
 
 
+"""
 def main():
-    pass
 
+    node = None
+    stack = [randint(1, 100) for _ in range(5)]
+    print(stack)
+    time.sleep(0.5)
+    link = SingleLinkList()
+    for x in stack:
+        link.append(x)
+
+    main_ = link.tail
+
+    while main_:
+        print(main_.data)
+        main_ = main_.next
+    # for x in range(1, len(stack)):
+    #     node = Node(stack[x-1], node)
+
+    # while node is not None:
+    #     print(node.data)
+    #     node = node.next
 
 if __name__ == "__main__":
     main()
+"""
+
+
